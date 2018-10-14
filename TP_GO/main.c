deddeb77466ab0900b2c32da74cf0355a7c02c5f
faten96 <32636018+faten96@users.noreply.github.com>
2018-10-14 20:23:52 +0200

    déclarer les structures de données

--- TP_GO/main.c
+++ TP_GO/main.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//décalaration des structures
+
+const int NbrObjet=5; /* nbr d'objets */
+const int PoidMax=10; /* poid maximal que peut supporter le sac à dos */
+int Valeurs[5]; /* table des valeurs des objets */
+int Poids[5];  /* table des poids des objets */
+int Gain[5][5]; /* table de calcul de gain */
+
 int main()
 {
     printf("Hello world!\n");

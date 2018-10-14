6abb35eab918b767802b5ffd25fff742ac46cce4
faten96 <32636018+faten96@users.noreply.github.com>
2018-10-14 20:45:57 +0200

    fonction d'initialisation de la table de gain

--- TP_GO/main.c
+++ TP_GO/main.c
@@ -9,8 +9,21 @@ int Valeurs[5]; /* table des valeurs des objets */
 int Poids[5];  /* table des poids des objets */
 int Gain[5][5]; /* table de calcul de gain */
 
+//initialiser les cases par les valeurs là ou il y a des "0"
+void initialiser_Gain_Table(int taille){
+
+    int i=0;
+    int j=0;
+
+    /* initialiser la première ligne et colonne par des "0" */
+    for(i=0;i<taille;i++) Gain[i][0]=0;
+    for(j=0;j<taille;j++) Gain[0][j]=0;
+
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    initialiser_Gain_Table(5);
+    printf("%d",Gain[0][0]);
     return 0;
 }
